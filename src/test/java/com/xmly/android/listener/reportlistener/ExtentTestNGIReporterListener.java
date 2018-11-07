a255aa9f850db6c5ad16ae8b788f9ffc255450eb
aliugenb <yeliu89@sina.com>
2018-11-07 19:42:10 +0800

    fix

--- src/test/java/com/xmly/android/listener/reportlistener/ExtentTestNGIReporterListener.java
+++ src/test/java/com/xmly/android/listener/reportlistener/ExtentTestNGIReporterListener.java
@@ -1,4 +1,4 @@
-package com.xmly.android.listener.testnglistener;
+package com.xmly.android.listener.reportlistener;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
