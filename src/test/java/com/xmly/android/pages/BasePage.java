41a00a786788799ed2f1dffedd7bc97baf68b4ad
aliugenb <yeliu89@sina.com>
2018-11-12 19:53:46 +0800

    fix

---
+++ src/test/java/com/xmly/android/pages/BasePage.java
@@ -0,0 +1,24 @@
+package com.xmly.android.pages;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import org.openqa.selenium.remote.RemoteWebElement;
+import io.appium.java_client.pagefactory.*;
+import org.openqa.selenium.support.PageFactory;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Author: ye.liu
+ * Date: 2018/11/12
+ * Time: 7:51 PM
+ */
+public class BasePage {
+
+    public AndroidDriver<AndroidElement> driver;
+
+    public BasePage(AndroidDriver<AndroidElement> driver) {
+        this.driver = driver;
+        //使用initElements方法构造,第一个参数new AppiumFieldDecorator,第二个参数是当前类
+        PageFactory.initElements(new AppiumFieldDecorator(driver), this);
+    }
+}
---
+++ src/test/java/com/xmly/android/pages/LiveIndexPage.java
@@ -0,0 +1,22 @@
+package com.xmly.android.pages;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import org.openqa.selenium.remote.RemoteWebElement;
+import io.appium.java_client.pagefactory.*;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Author: ye.liu
+ * Date: 2018/11/8
+ * Time: 11:09 AM
+ * 直播首页
+ */
+public class LiveIndexPage extends BasePage{
+
+    public LiveIndexPage(AndroidDriver<AndroidElement> driver) {
+        super(driver);
+    }
+
+
+}
