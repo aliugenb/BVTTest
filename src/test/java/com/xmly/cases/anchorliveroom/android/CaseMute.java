ced4f19e10d8f5f539e1d373cce3fcad73e96e19
aliugenb <yeliu89@sina.com>
2019-03-05 16:50:11 +0800

    fix

--- src/test/java/com/xmly/cases/anchorliveroom/android/CaseMute.java
+++ src/test/java/com/xmly/cases/anchorliveroom/android/CaseMute.java
@@ -11,25 +11,24 @@ import org.testng.annotations.Test;
  * Time: 15:47
  */
 public class CaseMute extends CaseHelper {
-
     @Test(description = "检查静音功能正常")
-    public void checkMute() throws InterruptedException {
+    public void checkMute() {
         createAnchorLiveRoom();
         anchorRoomIndexPage.enableMute();
         assertHelper.assertTrue(DriverHelper.isDisplayed(anchorRoomIndexPage.disableMuteBtn),
-                "CaseMute开启静音模式按钮文案变成静音");
+                getCurClassName() + "开启静音模式按钮文案变成静音");
 
         int width = driver.manage().window().getSize().width;
         int height = driver.manage().window().getSize().height;
         DriverHelper.clickByCoordinates(driver, width / 2, height / 3);
         assertHelper.assertTrue(DriverHelper.isDisplayed(anchorRoomIndexPage.micOffIcon),
-                "CaseMute开启静音模式出现静音icon");
+                getCurClassName() + "开启静音模式出现静音icon");
 
         anchorRoomIndexPage.disableMute();
         assertHelper.assertTrue(DriverHelper.isDisplayed(anchorRoomIndexPage.enableMuteBtn),
-                "CaseMute关闭静音模式按钮文案变成开麦");
+                getCurClassName() + "关闭静音模式按钮文案变成开麦");
         DriverHelper.clickByCoordinates(driver, width / 2, height / 3);
         assertHelper.assertTrue(!DriverHelper.isDisplayed(anchorRoomIndexPage.micOffIcon),
-                "CaseMute关闭静音模式静音icon消失");
+                getCurClassName() + "关闭静音模式静音icon消失");
     }
 }
