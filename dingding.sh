d48f6182060b614196184b0f2794e5c1a978436b
aliugenb <yeliu89@sina.com>
2019-03-08 16:22:57 +0800

    fix

---
+++ dingding.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+curl 'https://oapi.dingtalk.com/robot/send?access_token=bd1124386fa17ac91e9f8386d59d98a2dd199746f200c6be07b4c6303e0f85e1' \
+   -H 'Content-Type: application/json' \
+   -d '
+  {"msgtype": "text",
+    "text": {
+        "content": "我就是我, 是不一样的烟火"
+     },
+     "at": {
+         "atMobiles": [
+             "18918955916"
+         ],
+         "isAtAll": false
+     }
+  }'
